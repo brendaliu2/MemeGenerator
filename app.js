ab8b7531821d10c0aae88c2585ef977e3f9c5b31
Brenda Liu <brenda.liu24@gmail.com>
2022-06-28 20:35:24 -0500

    create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+const memeForm = document.querySelector('#memeForm');
+const memesContainer = document.querySelector('#memes');
+memeForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const imgURLInput = memeForm.elements.imageURL;
+    const topTextInput = memeForm.elements.topText;
+    const bottomTextInput = memeForm.elements.bottomText;
+    addMeme(topTextInput.value, bottomTextInput.value, imgURLInput.value);
+    imgURLInput.value = '';
+    topTextInput.value = '';
+    bottomTextInput.value = '';
+});
+
+const addMeme = (topText, bottomText, imageURL) => {
+    const newMeme = document.createElement('div');
+    newMeme.className = 'meme';
+    const top = document.createElement('div');
+    top.className = 'top';
+    const bottom = document.createElement('div');
+    bottom.className = 'bottom';
+    const memeImg = document.createElement('img');
+    memeImg.src = `${imageURL}`;
+    memeImg.alt = `Image not displaying. Please remove and try again.`;
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Remove';
+    deleteButton.className = 'btn btn-outline-dark btn-sm';
+    top.append(topText)
+    bottom.append(bottomText)
+    newMeme.append(memeImg);
+    newMeme.append(top);
+    newMeme.append(bottom);
+    newMeme.append(deleteButton);
+    memesContainer.append(newMeme);
+}
+
+
+memesContainer.addEventListener('click',function(e){
+  e.preventDefault();
+  const isButton = e.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+  let xButton = e.target;
+  let totalMeme = xButton.parentElement;
+  totalMeme.remove();
+})
